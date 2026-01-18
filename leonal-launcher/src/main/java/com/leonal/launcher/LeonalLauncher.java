4965652d16797bbe990eb08a80a9e09f901f1334
Andres Correa <andrescorreag@hotmail.com>
2026-01-18 17:01:30 -0500

    proyecto arranca con JavaFX + Spring + Flyway

--- leonal-launcher/src/main/java/com/leonal/launcher/LeonalLauncher.java
+++ leonal-launcher/src/main/java/com/leonal/launcher/LeonalLauncher.java
@@ -5,22 +5,22 @@ import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "com.leonal")
+@EnableJpaRepositories(basePackages = "com.leonal.infrastructure.persistence.repository")
+@EntityScan(basePackages = "com.leonal.infrastructure.persistence.entity")
 public class LeonalLauncher {
 
   public static ConfigurableApplicationContext context;
 
   public static void main(String[] args) {
-    // Start Spring Boot
     context = new SpringApplicationBuilder(LeonalLauncher.class)
-        .headless(false) // Allow UI
+        .headless(false)
         .run(args);
 
-    // Configure JavaFX Wiring
     JavaFXMain.setControllerFactory(context::getBean);
-
-    // Start JavaFX
     Application.launch(JavaFXMain.class, args);
   }
 }
